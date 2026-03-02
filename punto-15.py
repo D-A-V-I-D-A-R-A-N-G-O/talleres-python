3650029c03457de6f523bb8ec022863ee6eb8a6d
D-A-V-I-D-A-R-A-N-G-O <davidpeewee01241@outlook.com>
2026-03-02 18:11:16 -0500

    falta corregir

---
+++ punto-15.py
@@ -0,0 +1,10 @@
+num = float(input())
+x0 = num / 2
+x = 0.5 * (x0 + (num/x0))
+ite = 1
+while abs(x - x0) > 10**-12:
+    print(f"Iteracion {ite}, x_(n+1) = {x}, x_(n) = {x0}")
+    ite += 1
+    x0 = x
+    x = 0.5 * (x0 + (num/x0))
+print(f"La raiz cuadrada de {num} es {x} calculada en {ite} iteraciones")

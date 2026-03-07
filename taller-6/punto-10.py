1cc02df1671fe823001a5d587d9dd23303705363
D-A-V-I-D-A-R-A-N-G-O <davidpeewee01241@outlook.com>
2026-03-06 23:02:56 -0500

    facil tambien
    debo mejorar la atención, por ejemplo dejar de ver películas mientras hago los puntos

---
+++ taller-6/punto-10.py
@@ -0,0 +1,16 @@
+longi = int(input())
+lista = []
+lista1 = []
+final = ""
+for i in range(longi):
+    x = int(input())
+    lista.append(x)
+for i in range(longi):
+    x = int(input())
+    lista1.append(x)
+
+invertida = lista1[::-1]
+for i in range(longi):
+    print(lista[i], invertida[i])
+    final += str(lista[i]*invertida[i]) + " "
+print(final)

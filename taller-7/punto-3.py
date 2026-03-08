63a49fccabf551c78fd469e4eaf9858b12dd0bb7
D-A-V-I-D-A-R-A-N-G-O <davidpeewee01241@outlook.com>
2026-03-08 15:47:11 -0500

    superfacil, es más mi pendejada que la complejidad del problema

---
+++ taller-7/punto-3.py
@@ -0,0 +1,13 @@
+x  = int(input())
+def fibonacci(x):
+    a = 0
+    b = 1
+    c = 0
+    impri = ""
+    for i in range(x):
+        c = a + b
+        impri += str(a) + " "
+        a = b
+        b = c
+    return impri
+print(fibonacci(x))

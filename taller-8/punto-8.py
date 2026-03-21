2a484013ea8a3b7b7e0d0373d15669c6fdc98b01
D-A-V-I-D-A-R-A-N-G-O <davidpeewee01241@outlook.com>
2026-03-21 13:11:08 -0500

    listo, que bonita solucion, estara mal optimizada, pero esta divina

--- taller-8/punto-8.py
+++ taller-8/punto-8.py
@@ -1,13 +1,19 @@
 numero = int(input())
-contador_interno = int((numero-4)/2)
-contador_externo = 0
+c_i = int((numero-4)/2)
+c_e = 0
+c_i2 = 0
+c_e2 = int((numero-4)/2)
 for i in range(numero):
     if i+1 == numero/2 or i+1 == numero/2+1:
         print("*"*numero)
-    elif i+1 == numero/2-1 or i+1 == numero/2+2:
+    elif i+1 == numero/2-1 or i+1 == numero/2+1:
         p = int((numero-4)/2)
         print(" "*p + "****")
     elif i < int(numero/2-2):
-        print("*"+" "*contador_interno+ "**" + " "*contador_interno +"*")
+        print(" "*c_e + "*" + " "*c_i + "**" + " " *c_i + "*")
+        c_i -= 1
+        c_e += 1
     elif i > int(numero/2):
-        print("*"+" "*contador_interno+ "**" + " "*contador_interno +"*")
+        print(" "*c_e2 + "*" + " "*c_i2 + "**" + " " *c_i2 + "*")
+        c_i2 += 1
+        c_e2 -= 1

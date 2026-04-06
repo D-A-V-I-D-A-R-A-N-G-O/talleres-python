0d4797a37bb4b087179802320b07212a80bef246
D-A-V-I-D-A-R-A-N-G-O <davidpeewee01241@outlook.com>
2026-04-06 10:29:23 -0500

    purga

--- taller-9/punto-9.py
+++ taller-9/punto-9.py
@@ -1,10 +1,6 @@
 expresiones_opositivas = ["por otro lado", "a pesar de", "en cambio", "mientras que", "no solo", "sin embargo"]
 expresiones_causativas = ["hace que", "obligar a", "convencer a", "inducir a", "permitir que"]
-conteo_causativas = 0
-conteo_opositivas = 0
-otro = []
-delimite = " "
-delimiter = "."
+
 with open('discursos.txt', 'r', encoding='utf-8') as discurso:
     for linea in discurso:
         cc = 0

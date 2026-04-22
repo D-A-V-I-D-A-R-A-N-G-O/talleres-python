121ad4bcd52ed9f14c6ae92ac1d149c2252c102a
D-A-V-I-D-A-R-A-N-G-O <davidpeewee01241@outlook.com>
2026-04-22 11:35:15 -0500

    como se me olvido montar esto

---
+++ taller-10/punto-1.py
@@ -0,0 +1,19 @@
+longitud = int(input())
+lista = []
+for i in range(longitud**2):
+    x = int(input())
+    lista.append(x)
+
+for i in range(0, len(lista)-1, longitud):
+    if lista[i] != 1:
+        x = "Matriz no identidad"
+    else:
+        x = "Matriz identidad"
+    lista.pop(i)
+print(lista)
+for i in lista:
+    if i == 1:
+        x = "Matriz no identidad"
+        break
+
+print(x)

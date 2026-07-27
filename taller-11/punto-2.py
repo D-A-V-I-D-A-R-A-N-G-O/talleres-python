06ec73270702971e63b045e0e3871d26d0b67b05
D-A-V-I-D-A-R-A-N-G-O <davidpeewee01241@outlook.com>
2026-07-26 21:56:27 -0500

    kiss

--- taller-11/punto-2.py
+++ taller-11/punto-2.py
@@ -1,3 +1,27 @@
+import datetime
 x = int(input())
 
 for i in range(x):
+    r = input()
+    fecha = datetime.datetime.strptime(r, "%Y-%m-%d %H:%M:%S")
+    dia = fecha.isoweekday()
+    hora = int(datetime.datetime.strftime(fecha, "%H%M"))
+    if dia == 2:
+        d = "Martes"
+        if (hora >= 0 and hora <= 459) or (hora >= 2300 and hora <= 2359):
+            engagament = "Engagement medio bajo"
+        elif (hora >= 500 and hora <= 659) or (hora >= 1600 and hora <= 2359):
+            engagament = "Engagement medio"
+        elif (hora >= 700 and hora <= 759) or (hora >= 1400 and hora <= 1559):
+            engagament = "Engagement medio alto"
+        elif (hora >= 800 and hora <= 1360):
+            engagament = "Engagement alto"
+        else:
+            engagament = "No "
+    else:
+        d = "Otro dia"
+        if (hora >= 0 and hora <= 459) or (hora >= 2300 and hora <= 2359):
+            engagament = "Engagement bajo"
+        else:
+            engagament = "Sin informacion"
+    print(f"{d}: {engagament}")
